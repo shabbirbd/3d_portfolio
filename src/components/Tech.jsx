33bf624b05befe245fe91d91fd97382abba31944
shabbirbd <shabbirhossainshah@gmail.com>
2024-03-28 20:03:05 +0600

    tech section done

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { technologies } from '../constants'
+import { BallCanvas } from "./canvas"
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology, i)=>(
+        <div key={technology.name} className='w-28 h-28'>
+            <BallCanvas icon={technology.icon}/>
+        </div>
+      ))}
+    </div>
   )
 }
 
