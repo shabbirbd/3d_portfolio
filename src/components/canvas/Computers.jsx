6c7a9ca1ad53ff6941c33f731127f6109411e9c7
shabbirbd <shabbirhossainshah@gmail.com>
2024-03-30 18:58:27 +0600

    some color correction and changed 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,9 +22,9 @@ const Computers = ({ isMobile }) => {
       <ambientLight intensity={2}/>
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 4 : 6}
+        position={isMobile ? [0, -3, -0.2] : [0, -3.25, -0.5]}
+        rotation={[-0.01, -0.2, 0]}
       />
     </mesh>
   );
